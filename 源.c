203c5246ebc1aed8cc0e75cc05a8ac344dabdffb
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-03 18:17:35 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,35 +1,26 @@
+#define aqrt
 #include<stdio.h>
 int main()
 {
-	char c1, c2;
-	printf("请输入大写");
-	scanf_s("%c", &c1);
-	c2 = c1 + 32;
-	printf("输出小写%c\n", c2);
-	return 0;
+    double a, b, c, s=0, area;
+    a = 3.67;
+    b = 5.43;
+    c = 6.21;
+    s = (a + b + c) / 2;
+    area=aqrt(s*(s-a)*(s-b)*(s-c));
+    printf("%f\t%f\t%f\t", a, b, c);
+    printf("area=%f\n", area);
+    return 0;
 }
 #include<stdio.h>
 int main()
 {
-	int a,c;
-	scanf_s("%d", &a);
-	c = a / 1000;
-	printf("个位c=%d", c);
-	return 0;
-//}
-#include<stdio.h>
-int main()
-{
-	printf("你好 \n");
-	return 0;
-}
-#include<stdio.h>
-int main()
-{
-	int a, b, c;
-	printf("请输入长方形的长和高");	
-	scanf_s("%d%d", &a,&b);
-	c = a * b;
-	printf("长方形的面积为=%d",c);
+	int a;
+	printf("输入一个整数");
+	scanf_s("%d",&a);
+	if (a%2)
+		printf("a是奇数");
+	else
+		printf("a是偶数");
 	return 0;
 }

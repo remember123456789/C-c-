74865c54088cf3e4a4a4faf35ae8a89c3fefa675
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-03 12:43:16 +0800

    Add files via upload

---
+++ 123/123/源.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,f;
+	scanf_s("%d%d%d%d",&a,&b,&c,&d);
+	e = a + b + c + d;
+	printf("总成绩是%d\n",e);
+	f=e/4;
+	printf("平均成绩是%d\n",f);
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int a, b, c, d;
+	printf("请输入4个整数");
+	scanf_s("%d%d%d%d",&a,&b,&c,&d);
+	printf("")
+	return 0;
+}

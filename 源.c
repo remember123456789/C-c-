2760dd9f41f551fb62c1cf74c78f47f555109071
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-04 19:38:17 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,26 +1,14 @@
-#define aqrt
-#include<stdio.h>
-int main()
-{
-    double a, b, c, s=0, area;
-    a = 3.67;
-    b = 5.43;
-    c = 6.21;
-    s = (a + b + c) / 2;
-    area=aqrt(s*(s-a)*(s-b)*(s-c));
-    printf("%f\t%f\t%f\t", a, b, c);
-    printf("area=%f\n", area);
-    return 0;
-}
 #include<stdio.h>
 int main()
 {
 	int a;
-	printf("è¾“å…¥ä¸€ä¸ªæ•´æ•°");
-	scanf_s("%d",&a);
-	if (a%2)
-		printf("aæ˜¯å¥‡æ•°");
+	printf("ÇëÊäÈëaµÄÖµ");
+	scanf_s("%d", &a);
+	if (a == 0)
+		printf("¸ÃÕûÊıÊÇ0");
+	else if (a > 0)
+		printf("¸ÃÕûÊıÊÇÕıµÄ");
 	else
-		printf("aæ˜¯å¶æ•°");
+		printf("¸ÃÕûÊıÊÇ¸ºµÄ");
 	return 0;
 }

5b283412e1e65a78433ce6888835654ecafef259
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-13 08:29:55 +0800

    Add files via upload

---
+++ 源1.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	char a = 'B', b = 'O', c = 'Y';
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar('\n');
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int a = 66, b = 79, c = 89;
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar('\n');
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int a, b, c;
+	a = getchar();
+	b = getchar();
+	c = getchar();
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar('\n');
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	char c1, c2;
+	c2 = c1 + _32;
+	scanf_s("%c\n", c1);
+	printf("´óĞ´×ÖÄ¸ÊÇ%c", c2);
+	return 0;
+}

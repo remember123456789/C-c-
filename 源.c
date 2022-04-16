054c383030428a1f029124845ff8a56e167e072a
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-16 16:27:27 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,32 +1,52 @@
-#define MAX
 #include<stdio.h>
 int main()
 {
-	int a, b,c;
-	printf("请输入a和b的值");
-	scanf_s("%d%d", &a, &b);
-	c = MAX(a, b);
-	printf("最大值是%d", c);
+	float a, b, c, t;
+	printf("请输入3个整数");
+	scanf_s("%f%f%f", &a, &b, &c);
+	if (a > b)
+	{
+		t = a;
+		a = b;
+		b = t;
+	}
+	if (a > c)
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+	if (b > c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+	printf("%5.2f%5.2f%5.2f",a,b,c);
+	return 0;
+}
+#include<stdio.h>
+int main()
+{		
+	char ch;
+	scanf_s("%c",&ch);
+	ch = (ch >= 'A' && ch <= 'Z') ? (ch + 32) : ch;
+	printf("%c\n", ch);
 	return 0;
 }
 #include<stdio.h>
 int main()
 {
-	int a, b;
-	printf("请输入两个数的值");
-	scanf_s("%d%d", &a, &b);
-	printf("他们的差是%d", (a > b) ? a - b : b - a);
-	return 0;
-}
-#include<stdio.h>
-int main()
-{
-	int a;
-	printf("请输入一个整数");
-	scanf_s("%d", &a);
-	if (a % 5)
-		printf("输入的整数不能被5整除");
-	else
-		printf("输入的整数能被5整除");
+	char ch;
+	printf("请输入一个字符");
+	ch = getchar();
+	if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z')
+	printf("输入的字符是字母");
+	else  if (ch >= '0' && ch <= '9')
+	        printf("输入的字符是数字");
+	      else  if (ch == ' ')
+		        printf("该字符是空格");
+	            else
+		             printf("该字符不存在");
 	return 0;
 }

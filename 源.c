4af7fe18ead417155effe88471364625f76d6876
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-04-20 09:50:44 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,52 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	char garde;
+//	scanf("%c", &garde);
+//	printf("输入你的等级");
+//	switch (garde)
+//	{    
+//	     case 'a ':
+//	     case 'A': printf("85~100\n"); break;
+//		 case 'a' :
+//		 case 'B': printf("70~84\n"); break;
+//		 case 'c' :
+//		 case 'C': printf("60~69\n"); break;
+//		 case 'd' :
+//		 case 'D': printf("<60\n");  break;
+//		 default:printf("输入错误");
+//			 
+//	}
+//	return 0;
+//#include<stdio.h>
+//int main()
+//{
+//	float score;
+//	printf("请输入你的成绩");
+//	scanf("%f", &score);
+//	if (score > 100)
+//		printf("你是神仙吧");
+//	else 
+//	switch ((int)(score / 10))
+//	{
+//	case 10:
+//	case 9: printf("优秀"); break;
+//	case 8: printf("优秀"); break;
+//	case 7: printf("良好"); break;
+//	case 6: printf("及格"); break;
+//	default:printf("不及格");
+//	}
+//	return 0;
+//}
 #include<stdio.h>
 int main()
 {
-	float a, b, c, t;
-	printf("请输入3个整数");
-	scanf_s("%f%f%f", &a, &b, &c);
-	if (a > b)
-	{
-		t = a;
-		a = b;
-		b = t;
-	}
-	if (a > c)
-	{
-		t = a;
-		a = c;
-		c = t;
-	}
-	if (b > c)
-	{
-		t = b;
-		b = c;
-		c = t;
-	}
-	printf("%5.2f%5.2f%5.2f",a,b,c);
-	return 0;
-}
-#include<stdio.h>
-int main()
-{		
-	char ch;
-	scanf_s("%c",&ch);
-	ch = (ch >= 'A' && ch <= 'Z') ? (ch + 32) : ch;
-	printf("%c\n", ch);
-	return 0;
-}
-#include<stdio.h>
-int main()
-{
-	char ch;
-	printf("请输入一个字符");
-	ch = getchar();
-	if (ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z')
-	printf("输入的字符是字母");
-	else  if (ch >= '0' && ch <= '9')
-	        printf("输入的字符是数字");
-	      else  if (ch == ' ')
-		        printf("该字符是空格");
-	            else
-		             printf("该字符不存在");
+	int a, b, max;
+	printf("请输入a和b的值");
+	scanf("%d%d", &a,&b);
+	(a > b) ? (max = a) : (max = b);
+	printf("最大值是%d", max);
 	return 0;
 }

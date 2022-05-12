ff4ed0ed205b38f1b79eb505f14d0904150e0066
remember123456789 <102144161+remember123456789@users.noreply.github.com>
2022-05-12 11:42:41 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,51 +1,50 @@
-#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//int main()
-//{
-//	char garde;
-//	scanf("%c", &garde);
-//	printf("输入你的等级");
-//	switch (garde)
-//	{    
-//	     case 'a ':
-//	     case 'A': printf("85~100\n"); break;
-//		 case 'a' :
-//		 case 'B': printf("70~84\n"); break;
-//		 case 'c' :
-//		 case 'C': printf("60~69\n"); break;
-//		 case 'd' :
-//		 case 'D': printf("<60\n");  break;
-//		 default:printf("输入错误");
-//			 
-//	}
-//	return 0;
-//#include<stdio.h>
-//int main()
-//{
-//	float score;
-//	printf("请输入你的成绩");
-//	scanf("%f", &score);
-//	if (score > 100)
-//		printf("你是神仙吧");
-//	else 
-//	switch ((int)(score / 10))
-//	{
-//	case 10:
-//	case 9: printf("优秀"); break;
-//	case 8: printf("优秀"); break;
-//	case 7: printf("良好"); break;
-//	case 6: printf("及格"); break;
-//	default:printf("不及格");
-//	}
-//	return 0;
-//}
 #include<stdio.h>
 int main()
 {
-	int a, b, max;
-	printf("请输入a和b的值");
-	scanf("%d%d", &a,&b);
-	(a > b) ? (max = a) : (max = b);
-	printf("最大值是%d", max);
+	int no;
+	printf("请输入一个整数");
+	scanf_s("%d", &no);
+	while (no > 0 && no!=1)
+	{
+		no--;
+		printf("%d\n", no);
+	}
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int no;
+	printf("请输入一个整数");
+	scanf_s("%d", &no);
+	while (no-- >= 0)
+		putchar('*');
+	putchar('\n');
+	return 0;
+}
+#include<stdio.h>
+int main()
+{
+    int i, no;
+    printf("请输入一个整数");
+    scanf_s("%d",&no);
+    for (i = 1; i < no; i++)
+    {
+        putchar('*');
+        putchar('\n');
+    }
+    return 0;
+}
+#include<stdio.h>
+int main()
+{
+	int i=100 ;
+	printf("请输入一个100-300之间的整数");
+	
+	for (i; i < 300; i++)
+	{
+		if ((i % 4)==0) continue;
+		printf("%d\n", i);
+	}
 	return 0;
 }
